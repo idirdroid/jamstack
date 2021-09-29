6e8a3d76a04da14521fc7c4c71101f710062efa6
Idir ABED <abedidir@gmail.com>
2021-09-29 14:24:46 +0200

    Section admin

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 module.exports = function (eleventyConfig) {
     // On copie le css directement vers la sortie
-    eleventyConfig.addPassthroughCopy("style.css");
+    eleventyConfig.addPassthroughCopy("/style.css");
+    eleventyConfig.addPassthroughCopy("admin/config.yml");
+    eleventyConfig.addPassthroughCopy("images/background.jpg");
 };
