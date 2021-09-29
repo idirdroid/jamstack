74ff57af6cc5d0fb103944533cc9eb6f864f5a0e
Idir ABED <abedidir@gmail.com>
2021-09-29 15:33:12 +0200

    style css

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function (eleventyConfig) {
     // On copie le css directement vers la sortie
-    eleventyConfig.addPassthroughCopy("/style.css");
+    eleventyConfig.addPassthroughCopy("style.css");
     eleventyConfig.addPassthroughCopy("admin/config.yml");
     eleventyConfig.addPassthroughCopy("images/background.jpg");
 };
